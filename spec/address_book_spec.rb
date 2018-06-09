f0a352bef2fb550527e2bdb23ed1b9acdc09fd43
felicia <felicia@hyperarts.com>
2018-06-08 19:01:35 -0700

    Test against another CSV file.

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -45,34 +45,75 @@ RSpec.describe AddressBook do
             expect(book.entries.size).to eq(5)
         end
 
-        it "imports the first entry" do
+        it "imports the first entry from entries" do
             book.import_from_csv("entries.csv")
             entry = book.entries[0]
             check_entry(entry, "Bill", "555-555-4854", "bill@blocmail.com")
         end
 
-        it "imports the second entry" do
+        it "imports the second entry from entries" do
             book.import_from_csv("entries.csv")
             entry = book.entries[1]
             check_entry(entry, "Bob", "555-555-5415", "bob@blocmail.com")
         end
 
-        it "imports the third entry" do
+        it "imports the third entry from entries" do
             book.import_from_csv("entries.csv")
             entry = book.entries[2]
             check_entry(entry, "Joe", "555-555-3660", "joe@blocmail.com")
         end
 
-        it "imports the fourth entry" do
+        it "imports the fourth entry from entries" do
             book.import_from_csv("entries.csv")
             entry = book.entries[3]
             check_entry(entry, "Sally", "555-555-4646", "sally@blocmail.com")
         end
 
-        it "imports the fifth entry" do
+        it "imports the fifth entry from entries" do
             book.import_from_csv("entries.csv")
             entry = book.entries[4]
             check_entry(entry, "Sussie", "555-555-2036", "sussie@blocmail.com")
+        end
+
+        it "imports the correct number of entries from entries_2" do
+            book.import_from_csv("entries_2.csv")
+            expect(book.entries.size).to eq(6)
+        end
+
+        it "imports the first entry in alphabetical order from entries_2" do
+            book.import_from_csv("entries_2.csv")
+            entry = book.entries[3]
+            check_entry(entry, "Philomena", "555-555-1111", "philly_girl@blocmail.com")
+        end
+
+        it "imports the second entry in alphabetical order from entries_2" do
+            book.import_from_csv("entries_2.csv")
+            entry = book.entries[0]
+            check_entry(entry, "Jared", "555-555-2222", "jaredy@blocmail.com")
+        end
+
+        it "imports the third entry in alphabetical order from entries_2" do
+            book.import_from_csv("entries_2.csv")
+            entry = book.entries[2]
+            check_entry(entry, "Meghan", "555-555-3333", "mgj@blocmail.com")
+        end
+
+        it "imports the fourth entry in alphabetical order from entries_2" do
+            book.import_from_csv("entries_2.csv")
+            entry = book.entries[1]
+            check_entry(entry, "Jenna", "555-555-4444", "jenna.bentham@blocmail.com")
+        end
+
+        it "imports the fifth entry in alphabetical order from entries_2" do
+            book.import_from_csv("entries_2.csv")
+            entry = book.entries[5]
+            check_entry(entry, "Zarathustra", "555-555-5555", "thebigz@blocmail.com")
+        end
+
+        it "imports the sixth entry in alphabetical order from entries_2" do
+            book.import_from_csv("entries_2.csv")
+            entry = book.entries[4]
+            check_entry(entry, "Waldomir", "555-555-6666", "wally_boy@blocmail.com")
         end
     end
 end
