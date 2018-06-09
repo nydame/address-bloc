7062d537c51e54f9eba73ab4b4647753f0e391a3
felicia <felicia@hyperarts.com>
2018-06-09 11:48:17 -0700

    Checkpoint 5 - Searching: completed.

--- models/address_book.rb
+++ models/address_book.rb
@@ -33,4 +33,34 @@ class AddressBook
             add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
         end
     end
+
+    def search(name)
+        @entries.each do |entry|
+            return entry if entry.name == name
+        end
+        return nil
+    end
+
+    def binary_search(name)
+        # set lower and upper boundaries of search
+        lower = 0
+        upper = @entries.length - 1
+        # set up while loop
+        while lower <= upper
+            # find entry at mid-point and see if it matches input
+            mid = (lower + upper) / 2
+            mid_name = @entries[mid].name
+            if name == mid_name
+                return @entries[mid]
+            elsif name < mid_name
+                upper = mid - 1
+            elsif name > mid_name
+                lower = mid + 1
+            end
+
+
+        end
+        # if nothing found within while loop, name is not in AddressBook
+        return nil
+    end
 end
